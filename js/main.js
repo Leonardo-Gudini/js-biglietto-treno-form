e0ebb7d6cfff5afc8af0700a4e7b568c659a2f7a
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-07-05 18:56:54 +0200

    fixed codice calcolo biglietto

--- js/main.js
+++ js/main.js
@@ -3,32 +3,41 @@ const genera = document.getElementById("genera");
 genera.addEventListener("click",
 
     function(){
-        const nomeCognome = document.getElementById("nomeCognome")
-        alert("il tuo nome è: " + nomeCognome.value)
-
-        const chilometri = document.getElementById("chilometri")
-        alert("devi percorrere: " + chilometri.value + " Km")
-
-        const fasciaEta = document.getElementById("fasciaEta")
-        alert("La tua fascia d'età è: " + fasciaEta.value)
-
-        const prezzoBiglietto = parseInt(0.21 * chilometri.value);
+        
 
 
-        if(fasciaEta.value == "minorenne"){
+        const nomeCognome = document.getElementById("nomeCognome").value;
+        const nomePasseggero = document.getElementById('nomePasseggero');
+        nomePasseggero.innerHTML = nomeCognome;
 
+        const chilometri = parseInt(document.getElementById("chilometri").value);
+
+        const fasciaEta = document.getElementById("fasciaEta").value;
+
+        const prezzoBiglietto = parseInt(0.21 * chilometri);
+        const prezzo = document.getElementById("prezzo");
+
+        const tipologiaBiglietto = document.getElementById("tipologiaBiglietto");
+
+        const carrozza = document.getElementById("carrozza");
+        carrozza.innerHTML = Math.floor(Math.random() * 10) + 1;
+
+        const prenotazione = document.getElementById("prenotazione");
+        prenotazione.innerHTML = Math.floor(Math.random() * 90000) + 9999;
+
+         if(fasciaEta == "minorenne"){
             const sconto20 = prezzoBiglietto / 100 * 20;
+            tipologiaBiglietto.innerHTML = "Sconto Young"
+            prezzo.innerHTML = (prezzoBiglietto - sconto20).toFixed(2);
 
-            alert("Il prezzo del tuo biglietto è " + (prezzoBiglietto - sconto20) .toFixed(2));
-
-        } else if (fasciaEta.value == "over65") {
-
+        } else if (fasciaEta == "over65") {
             const sconto40 = prezzoBiglietto / 100 * 40;
-            alert("Il prezzo del tuo biglietto è " + (prezzoBiglietto - sconto40) .toFixed(2));
+            tipologiaBiglietto.innerHTML = "Sconto Silver"
+            prezzo.innerHTML = (prezzoBiglietto - sconto40).toFixed(2);
             
         } else {
-
-            alert("Il prezzo del tuo biglietto è " + prezzoBiglietto);
+            prezzo.innerHTML = prezzoBiglietto.toFixed(2);
+            tipologiaBiglietto.innerHTML = "Standard"
         }
     }
 );
